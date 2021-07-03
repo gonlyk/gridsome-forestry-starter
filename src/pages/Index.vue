1faf5d58b28affe328df26c6aa47ff2523d261fc
gonlyk <gonlyk@163.com>
2021-07-04 02:04:44 +0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <Layout>
+    <div class="container">
+      <Hero />
+      <ProjectsGrid :projects="$page.project.edges" />
+    </div>
+    <LatestJournals :journals="$page.journal.edges" />
+  </Layout>
+</template>
+
+<page-query>
+query {
+	project: allStrapiProject {
+    edges {
+      node {
+        id
+        date (format: "YYYY")
+        title
+        categories
+        thumbnail {
+          url
+        }
+        path
+      }
+    }
+  },
+  journal: allStrapiJournalPost (perPage: 4) {
+    edges {
+      node {
+        id
+        path
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Hero from "@/components/Hero"
+import ProjectsGrid from "@/components/ProjectsGrid"
+import LatestJournals from "@/components/LatestJournals"
+export default {
+  components: {
+    Hero,
+    ProjectsGrid,
+    LatestJournals
+  }
+}
+</script>
